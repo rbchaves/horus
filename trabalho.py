11231b2c1834650b679cf2b16580ab82d153dce4
rbchaves <raphael.balmant@hotmail.com>
2017-06-22 12:31:34 -0300

    Add files via upload

--- trabalho.py
+++ trabalho.py
@@ -14,8 +14,8 @@ from tkMessageBox import showerror
 # sys.exit()
 import sys
 # Twitter
+from twitter import *
 import tweepy
-
 
 CONFIG_MAP_PATH_TO_FILE = "map.ini"
 CONFIG_TWITTER_PATH_TO_FILE = "twitter.ini"
@@ -59,7 +59,6 @@ class Map:
 	def plot_point(self, lat, lon, radius):
 		x, y = self.map(lon, lat)
 		self.map.plot(x, y, 'bo', markersize=radius)
-		plt.draw()
 
 class AuthHandler:
 	""" Responsável pelo processo de autenticação do Twitter """
@@ -67,6 +66,7 @@ class AuthHandler:
 		self.connected = False
 		self.__config = Config(config_file)
 		self.__auth = tweepy.OAuthHandler(self.__config.get_config("CONSUMER_KEY"), self.__config.get_config("CONSUMER_SECRET"))
+		self.twitter = Twitter(auth = OAuth(self.__config.get_config("ACCESS_TOKEN"), self.__config.get_config("ACCESS_TOKEN_SECRET"), self.__config.get_config("CONSUMER_KEY"), self.__config.get_config("CONSUMER_SECRET")))
 		self.__auth.set_access_token(self.__config.get_config("ACCESS_TOKEN"), self.__config.get_config("ACCESS_TOKEN_SECRET"))
 		self.api = tweepy.API(self.__auth)
 		self.connected = True
@@ -74,10 +74,12 @@ class AuthHandler:
 class TrendsStreamListener(tweepy.StreamListener):
 	def __init__(self, auth_handler):
 		self.api = auth_handler.api
+		self.twitter = auth_handler.twitter
 		self.trends_round = 0
 		self.map = Map(CONFIG_MAP_PATH_TO_FILE)
 		self.stream = tweepy.Stream(auth=self.api.auth, listener=self)
 		self.get_new_trends()
+		self.__set_timer()
 	
 	def get_new_trends(self):
 		self.trends_round += 1
@@ -86,20 +88,27 @@ class TrendsStreamListener(tweepy.StreamListener):
 		trends = trends_data['trends']
 		trends_name = [trend['name'] for trend in trends]
 		print trends_name
-		self.stream.filter(track=trends_name)
+		self.stream.filter(track=trends_name, async=True)
+		#t = self.twitter.tweets(q='#python')
+		#print t
 
 	def __set_timer(self):
-		self.__timer = Timer(300, self.get_new_trends)
+		self.__timer = Timer(30, self.redraw_map)
 		self.__timer.start()
+
+	def redraw_map(self):
+		plt.draw()
+		self.__timer.stop()
+		self.__set_timer()
 
 	def on_status(self, tweet):
 		if (tweet.coordinates != None):
-			print tweet.text, "\t", tweet.coordinates
+			print tweet.coordinates
 			self.map.plot_point(tweet.coordinates['coordinates'][1], tweet.coordinates['coordinates'][0], 5)
-			plt.pause(0.1)
-
+			plt.pause(1)
+			
 	def on_error(self, status):
-		print "[" + str(datetime.datetime.now()) + "] " + str(status)
+		print ("[" + str(datetime.datetime.now()) + "] " + str(status))
 
 # Apenas essa linha é executada enquanto o mapa está aberto
 """MY_MAP = Map()"""
@@ -108,7 +117,6 @@ class TrendsStreamListener(tweepy.StreamListener):
 MY_AUTH_HANDLER = AuthHandler()
 MY_MAP.plot_point(-33.924869, 18.424055, 5)
 print MY_AUTH_HANDLER.connected"""
-
 
 MY_AUTH_HANDLER = AuthHandler()
 TRENDS_STREAM_LISTENER = TrendsStreamListener(MY_AUTH_HANDLER)
